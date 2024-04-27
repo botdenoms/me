04de380849b6f8fc1090a1578ff68f9d58d0a301
Dennis moseti <dudubot404@gmail.com>
2024-04-27 17:05:17 +0300

    data: work data appended

---
+++ src/works.js
@@ -0,0 +1,117 @@
+export const worksList = [
+    {
+        title: 'Medeasy',
+        category: 'Mobile app',
+        thumbnail: 'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fmedeasy%20showcase%202.png?alt=media&token=d18a5a01-0c7e-4fb0-ba22-de799597f5f8',
+        description: `A mobile application offering remote or online diagnosis services.
+        This services is enabled by bringing all involved stakeholder:  
+        specialized doctor, health and test facilities and the users requiring the services. 
+        Through the app a user can find specialist, schedule tests, request diagnosis service 
+        , share test, view diagnosis and more. specialist and facilities can register to offer 
+        their services to the users.`,
+        tools: [
+            'Flutter',
+            'Firebase',
+            'Vite',
+            'Figma',
+        ],
+        showcase: [
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2FScreenshot_20240419-231057.png?alt=media&token=34a26967-e341-4cd1-b069-c01e245f0bfc',
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fmedesy%201%20.png?alt=media&token=20827d24-d52a-40b5-96b7-b15b3b12b0af', 
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2FScreenshot_20240419-231459.png?alt=media&token=51e871b5-422e-4f39-8d5e-10dcf1a1ca24', 
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2FScreenshot_20240419-231158.png?alt=media&token=5f70bf7b-4a4e-431a-99cf-0acaeeef3e50'
+        ],
+        stage: 'Development',
+        repo: 'https://github.com/botdenoms/medeasy',
+        otherlinks: [
+            // {link : '', store: ''}
+        ],
+        feedback: ''
+    },
+    {
+        title: 'Url2pdf',
+        category: 'Python Flask api',
+        thumbnail: 'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Furl2pdf%2Furl2pdf%20show.png?alt=media&token=3437ce59-a5e7-456c-9942-f26e29d0388d',
+        description: `Converts a given url links to a pdf file. Requests made to \n
+        the REST api accepts website urls as the parameter, \n
+        the api returns a json object. The json object contains the given url \n
+        website pdf document in base64 format, if the url is a valid url\n
+        the based64 encoding can be converted to a pdf file format.
+        `,
+        tools: [
+            'Python',
+            'Flask',
+            'Selenium',
+        ],
+        showcase: ['https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Furl2pdf%2Furl2pdf%202.png?alt=media&token=87e0f346-8b09-489b-9aad-5ecd566cd12e', 'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Furl2pdf%2Furl2pdf%201.png?alt=media&token=3a0bdbfd-896a-4b85-8458-41d37be62e72'],
+        stage: 'Production',
+        repo: 'https://github.com/botdenoms/url2pdf',
+        otherlinks: [],
+        feedback: ''
+    },
+    {
+        title: 'Portfolio',
+        category: 'Webiste',
+        thumbnail: 'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fportfolio%2Fportfolio%20showcs.png?alt=media&token=3cf68f33-87d6-4840-bf36-f766eeb99244',
+        description: `My portfolio website, a static website showcasing my works.
+        Contact links on how to find me and more information.
+        `,
+        tools: [
+            'Vite',
+            'Figma',
+        ],
+        showcase: [
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fportfolio%2Fportfolio%203.png?alt=media&token=53d7fdf5-5711-4449-ab60-eb3e578bb5c7', 
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fportfolio%2Fportfolio%202.png?alt=media&token=593f51a4-e54d-4bff-a289-6240ff3e5e24', 
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fportfolio%2Fportfolio%201.png?alt=media&token=f0c5a256-17b0-4da9-87dc-1e703c6a36c8'
+        ],
+        stage: 'Production',
+        repo: '',
+        otherlinks: [],
+        feedback: `My personal portfolio website, a simple, minimalistic website.\n
+        \n
+        `
+    },
+    {
+        title: 'Offerske',
+        category: 'Python app',
+        thumbnail: 'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fofferske%2Fofferske%20show.png?alt=media&token=08331cc7-08c7-451c-8044-bc41ce7fdf55',
+        description: `A Scrapy python app: using scrapy package, this program scraps flash sale offer 
+        on jumia e-commerce websit. \n
+        Supports exporting scrapped data in scapy supported types, json, csv, pickle. \n
+        plus custom support for mongodb pipeline for custom export to mongodb database.
+        `,
+        tools: [
+            'Python',
+            'Scrapy'
+        ],
+        showcase: [
+            'https://firebasestorage.googleapis.com/v0/b/dennismosetio.appspot.com/o/images%2Fofferske%2Foffers%202.png?alt=media&token=a2cb7a57-7992-4b0b-aa49-2529cfe5ca21'
+        ],
+        stage: 'Production',
+        repo: 'https://github.com/botdenoms/offerske',
+        otherlinks: [],
+        feedback: ''
+    },
+    {
+        title: 'Ranked Choice Voting, (RCV)',
+        category: 'Web app',
+        thumbnail: '',
+        description: `A web application: using the ranked choice voting methodology
+        Users can create elections, add candidates, share the election, vote on elections 
+        and view results plus visualizations of the data. 
+        Featuress include anonimous voting, open public or private voting.
+        `,
+        tools: [
+            'React',
+            'Vite',
+            'Firebase',
+            'Figma',
+        ],
+        showcase: [],
+        stage: 'Production',
+        repo: 'https://github.com/botdenoms/rcv',
+        otherlinks: [],
+        feedback: ''
+    },
+]
